8ca62d09cd6f6ad5d763e7f3908d900b2ea34968
taelgalli <taelgalli@gmail.com>
2024-05-12 22:04:54 -0300

    Adjustments after playtest: Improve popUp animation and improve background consistency

--- Assets/ClothShop/FadeInOnEnableBehavior.cs
+++ Assets/ClothShop/FadeInOnEnableBehavior.cs
@@ -5,28 +5,10 @@ using UnityEngine;
 public class FadeInOnEnableBehavior : MonoBehaviour
 {
 
-    [SerializeField] private float fadeInSpeed = 0.01f;
+    [SerializeField] private Animator animator;
 
     private void OnEnable() {
-        StartCoroutine(FadeInOnEnable());
+        animator.Play(0);
     }
-
-    private IEnumerator FadeInOnEnable() {
-        this.transform.localScale = Vector2.zero;
-
-        while (this.transform.localScale.x < 1.1f) {
-            this.transform.localScale = new Vector2(this.transform.localScale.x + fadeInSpeed, this.transform.localScale.y + fadeInSpeed);
-            yield return new WaitForEndOfFrame();
-        }
-
-        while (this.transform.localScale.x > 1f) {
-            this.transform.localScale = new Vector2(this.transform.localScale.x - fadeInSpeed, this.transform.localScale.y - fadeInSpeed);
-            yield return new WaitForEndOfFrame();
-        }
-        
-    }
-
-    
-
     
 }
